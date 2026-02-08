b207cfa8ec68775bcd8b7e907401c4a8b11f748c
DavidMelkadze <davidmelkadze23@gmail.com>
2026-02-08 16:04:25 +0400

    Initial Tenzies game setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
 import { useState } from 'react'
-
+import Die from './components/Die'
+import './index.css'
+import { nanoid } from 'nanoid'
 
 export default function App() {
-  
+
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() =>  ({ value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid()
+    }))
+  }
+
+  function rollDice() {
+    setDice(prevDice => prevDice.map(die => (
+     !die.isHeld ?
+      {...die, value:Math.ceil(Math.random() * 6)} :
+      die
+    )))
+  }
+
+  function hold(id) {
+    setDice(prevDice => prevDice.map(die => (
+      id === die.id ? 
+      {...die, isHeld: !die.isHeld} :
+      die
+    )))
+  }
+
+  const diceElements = dice.map(dieObj => 
+            <Die 
+                key={dieObj.id} 
+                value={dieObj.value} 
+                isHeld={dieObj.isHeld} 
+                hold={() => hold(dieObj.id)}
+            />)
+
   return (
     <>
-      
+      <main>
+        <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="dice-container">
+          {diceElements}
+        </div>
+
+        <button className="roll-dice" onClick={rollDice}>
+          Roll Dice
+        </button>
+
+      </main>
     </>
   )
 }
