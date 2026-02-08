e08ae7077343f19542f7bd305265dc795bcfa75a
DavidMelkadze <davidmelkadze23@gmail.com>
2026-02-08 17:58:27 +0400

    Finish Tenzies game with win logic, accessibility, and focus management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect, use } from 'react'
 import Die from './components/Die'
 import './index.css'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 export default function App() {
 
-  const [dice, setDice] = useState(generateAllNewDice())
+  const [dice, setDice] = useState(() => generateAllNewDice())
+
+  const buttonRef = useRef(null)
+
+  const gameWon = dice.every(die => die.isHeld && die.value === dice[0].value)
+
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus()
+    }
+  }, [gameWon])
 
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() =>  ({ value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid()
@@ -13,11 +24,15 @@ export default function App() {
   }
 
   function rollDice() {
+    if(!gameWon) {
     setDice(prevDice => prevDice.map(die => (
      !die.isHeld ?
       {...die, value:Math.ceil(Math.random() * 6)} :
       die
     )))
+  }   else {
+    setDice(generateAllNewDice())
+  }
   }
 
   function hold(id) {
@@ -39,14 +54,18 @@ export default function App() {
   return (
     <>
       <main>
+        {gameWon && <Confetti />}
+        <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
         <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="dice-container">
           {diceElements}
         </div>
 
-        <button className="roll-dice" onClick={rollDice}>
-          Roll Dice
+        <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
+          {gameWon ? "New Game" : "Roll Dice"}
         </button>
 
       </main>
